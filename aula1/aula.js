8a9ac206f75016be188acf62d1e69a626d2b547b
Rennan Augusto Ates dos Santos <rennanaugusto@MacBook-Air-de-Rennan.local>
2023-05-10 06:23:04 -0300

    Aula 1

---
+++ aula1/aula.js
@@ -0,0 +1,17 @@
+/*
+
+Faça um programa para calcular o valor gasto de combustivel uma viagem.
+
+Você terá três variáveis. Sendo elas:
+
+1 - Preço do combustível
+2 - Valor médio de gasto pelo carro;
+3 - Distância em km da viagem
+
+*/
+const precoCombustivel = 5.79;
+const medioCombustivel = 10;
+const distanciaViagem = 79;
+
+valorGasto = distanciaViagem / medioCombustivel * precoCombustivel
+console.log(valorGasto.toFixed(2));
