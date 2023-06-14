611545b867ff24fa2e682560f62f5fc632a5dc01
Rennan Augusto Ates dos Santos <rennanaugusto@MacBook-Air-de-Rennan.local>
2023-06-14 17:31:32 -0300

    call

--- closure/index.js
+++ closure/index.js
@@ -9,3 +9,15 @@ const SomaParcial = soma(20)
 
 console.log(SomaParcial(50))
 console.log(SomaParcial(70))
+
+
+const pessoa = {
+    nome : 'Rennan',
+    idade : 19
+}
+function gritar(prefixo) {
+    console.log(prefixo, this.nome)
+}
+
+
+gritar.call(pessoa, 'Olaaaa')
